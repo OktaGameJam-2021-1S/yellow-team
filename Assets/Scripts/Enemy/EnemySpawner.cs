4852117c3d75931e754e7a2a811ba1081caad666
FabianoOktagon <fabiano.marumo@oktagon.com.br>
2021-06-05 12:19:48 -0300

    new: enemy can target player and castle and spawn points

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private Transform castle;
+    
     private const float overlapSphereRadius = 0.5f;
     [Header("Spawn settings")]
     [SerializeField] private int enemyCount;
     [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private List<Transform> spawnPoints;
     private EnemyHandler enemyHandler;
+
 
     private void Awake()
     {
@@ -19,21 +23,47 @@ public class EnemySpawner : MonoBehaviour
     {
         if (enemies.Count == 0)
             throw new System.ArgumentNullException("enemies", "Лист префабов противников пуст");
+
+
         for (int i = 0; i < enemyCount; i++)
         {
             Vector3 spawnPos;
-            int loopBreaker = 0;
-            do
-            {
-                loopBreaker++;
-                spawnPos = new Vector3(Random.Range(-4f,4f), 0.51f, Random.Range(2f, 13f));//Значения вытаскиваются из конструктора поля или из данных о префабе поля, если игровые поля будут разного размера
-            } while (CheckCollisions(spawnPos) && loopBreaker<100); //Может войти в бесконечный цикл, если не будет места для спавна
-            if (loopBreaker < 100)
-            {
-                var newEnemy = Instantiate(enemies[Random.Range(0, enemies.Count)], transform);
-                newEnemy.transform.position = spawnPos;
-                enemyHandler.AddEnemy(newEnemy.GetComponent<Enemy>());
-            }
+            //int loopBreaker = 0;
+
+            // Spawn anywhere
+            //do
+            //{
+            //    loopBreaker++;
+            //    spawnPos = new Vector3(Random.Range(-4f,4f), 0.51f, Random.Range(2f, 13f));//Значения вытаскиваются из конструктора поля или из данных о префабе поля, если игровые поля будут разного размера
+            //} while (CheckCollisions(spawnPos) && loopBreaker<100); //Может войти в бесконечный цикл, если не будет места для спавна
+            //if (loopBreaker < 100)
+            //{
+            //    var newEnemy = Instantiate(enemies[Random.Range(0, enemies.Count)], transform);
+            //    newEnemy.transform.position = spawnPos;
+            //    enemyHandler.AddEnemy(newEnemy.GetComponent<Enemy>());
+            //}
+
+
+            spawnPos = spawnPoints[ i % spawnPoints.Count].position;
+            bool isPlayer = i % 2 == 0 ? true : false;
+
+            // TODO: Enemies should not be inside each other
+            //do
+            //{
+            //    loopBreaker++;
+            //    spawnPos = new Vector3(Random.Range(-4f, 4f), 0.51f, Random.Range(2f, 13f));//Значения вытаскиваются из конструктора поля или из данных о префабе поля, если игровые поля будут разного размера
+            //} while (CheckCollisions(spawnPos) && loopBreaker < 100); //Может войти в бесконечный цикл, если не будет места для спавна
+            //if (loopBreaker < 100)
+            //{
+            var newEnemy = Instantiate(enemies[Random.Range(0, enemies.Count)], transform);
+            newEnemy.transform.position = spawnPos;
+            enemyHandler.AddEnemy(newEnemy.GetComponent<Enemy>());
+            enemyHandler.Enemies[enemyHandler.Enemies.Count - 1].SetTarget(isPlayer, castle);
+            //}
+
+            // Set enemy target
+
+
         }
     }
 
