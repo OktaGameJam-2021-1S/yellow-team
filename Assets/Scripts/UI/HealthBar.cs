d0c0682d6477a219d17a3ad9d5072dc038af0c26
Renan Laureano <renanlaureano@gmail.com>
2021-06-06 10:15:11 -0300

    change: Improve HealthBar

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -3,10 +3,30 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-	[SerializeField] Image bar;
-	[SerializeField] Entity entity;
-	private void Update() // Можно написать лучше структуру, которая будет работать на событиях по изменению hp
-	{
-		bar.fillAmount = Mathf.Lerp(bar.fillAmount, entity.Hp / entity.MaxHp, 0.2f);
-	}
+    [SerializeField] Image bar;
+    [SerializeField] Entity entity;
+    [SerializeField] Transform targetPosition;
+    Transform target;
+
+    void Start()
+    {
+        target = Camera.main.transform;
+        transform.SetParent(null);
+    }
+    private void Update()
+    {
+        bar.fillAmount = Mathf.Lerp(bar.fillAmount, entity.Hp / entity.MaxHp, 0.2f);
+
+        if (entity.Hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (targetPosition != null)
+        {
+            transform.position = targetPosition.position;
+        }
+
+        transform.LookAt(target);
+    }
 }
